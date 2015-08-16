4280021106db1cd455eaf178cd85ea631a8350d1
RasPi2 Dashcam <kenchou77@gmail.com>
2015-08-16 18:04:38 +0800

    mv storage path to ~/Videos

--- dashcam.py
+++ dashcam.py
@@ -2,6 +2,7 @@
 
 __author__ = 'kzhang'
 
+import os.path
 import picamera
 from datetime import datetime
 
@@ -20,36 +21,43 @@ def update_annotate(camera, interval):
         camera.wait_recording(1)
 
 
-storage_path = '~/Videos'
+storage_path = '/home/pi/Videos'
 interval = 3 * 60   # how many minutes every file
 resolution = (1920, 1080)    # Full HD
 #resolution = (1296, 972)
 keep_going = True
 
 with picamera.PiCamera() as camera:
-    camera.resolution = resolution
-    camera.framerate = 30
-    camera.rotation = -90
-    #camera.hflip = True
-    #camera.vflip = True
-    filename = get_output_filename()
-
-    print 'Start recording', filename
-    camera.start_preview()
-    camera.start_recording(filename, format='h264')
-    #camera.annotate_text = get_annotate()
-    #camera.wait_recording(interval)
-    update_annotate(camera, interval)
-
-    # main loop
-    while keep_going:
+    try:
+        camera.resolution = resolution
+        camera.framerate = 30
+        camera.rotation = -90
+        #camera.hflip = True
+        #camera.vflip = True
         filename = get_output_filename()
+        output_file = os.path.join(storage_path, filename)
+
         print 'Start recording', filename
-        camera.split_recording(filename)
-        update_annotate(camera, interval)
+        camera.start_preview()
+        camera.start_recording(output_file, format='h264')
         #camera.annotate_text = get_annotate()
         #camera.wait_recording(interval)
+        update_annotate(camera, interval)
 
-    print 'Stop.'
-    camera.stop_recording()
+        # main loop
+        while keep_going:
+            filename = get_output_filename()
+            output_file = os.path.join(storage_path, filename)
+
+            print 'Start recording', filename
+            camera.split_recording(output_file)
+            update_annotate(camera, interval)
+            #camera.annotate_text = get_annotate()
+            #camera.wait_recording(interval)
+    except Exception as e:
+        print e
+    finally:
+        print 'Stop.'
+        camera.stop_recording()
+        camera.close()
 
