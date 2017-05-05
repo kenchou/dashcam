d01593ac6ad31aab62bc6b2e8ed6a5478eaea09f
Ken Chou <zhang-j@klab.com>
2017-05-05 14:08:58 +0800

    do not start if service actived

--- picam-stream-suppressor.sh
+++ picam-stream-suppressor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-arp-scan -l | grep 38:a4:ed:be:70:7c && systemctl stop picam-stream || systemctl start picam-stream
+arp-scan -l | grep 38:a4:ed:be:70:7c && systemctl stop picam-stream || systemctl status picam-stream || systemctl start picam-stream
 systemctl status picam-stream
